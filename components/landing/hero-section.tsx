bdd83f1589abbd18f3d9c069b58da3b8e3a99d16
Daniel916-boop <danielmacedodasilva39@gmail.com>
2025-08-29 10:33:10 -0300

    Update hero-section.tsx

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -104,7 +104,7 @@ export default function HeroSection() {
 
           <div className="max-w-3xl mx-auto mb-6 sm:mb-8">
             <AnimatedText
-              text="Te mintieron: la testosterona baja no es por la edad. Proviene de toxinas, del estrés y de hábitos que bloquean tu producción natural impidiendo así que tu lápiz crezca y que tú te sientas hombre. Pero la verdad es que tu cuerpo todavía puede producir testosterona como si tuvieras 20 años."
+              text="Te mintieron: la testosterona baja no es por la edad. Proviene de toxinas, del estrés y de hábitos que bloquean tu producción natural impidiendo así que tu lápiz crezca y que tú te sientas hombre. Pero la verdad es que tu cuerpo todavía puede producir testosterona como si si fueras un toro."
               className="text-base sm:text-lg md:text-xl text-white/80 mb-4 sm:mb-6"
               delay={1}
             />
